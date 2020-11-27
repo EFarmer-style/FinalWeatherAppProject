fc9814df1c6a52e79505a414a3e374995d54690b
EFarmer-style <72121424+EFarmer-style@users.noreply.github.com>
2020-11-27 16:59:05 +1000

    added city key

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "478e29e21dad691d21aa0e127ed51a72";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New Zealand";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
